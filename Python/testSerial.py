afaa96168de10abfa4b6eadbcac6a72260355439
coolshoeshine <o.enriquez89@gmail.com>
2020-09-24 22:03:36 -0700

    edited gitignore

--- Python/testSerial.py
+++ Python/testSerial.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial('COM3')  # open serial port
+ser = serial.Serial('COM3', 115200)  # open serial port
 print(ser.name)         # check which port was really used
 ser.write(b'hello')     # write a string
 ser.close()             # close port
