9dcc8bfc33726abe37145cfec648d09731ded375
coolshoeshine <o.enriquez89@gmail.com>
2020-09-29 20:08:59 -0700

    made verilog RS232_tx module

--- FPGA/library/RS232_tx.v
+++ FPGA/library/RS232_tx.v
@@ -1,25 +1,25 @@
 
-module RS232_rx(clk, rst, rx, dataOut, dataDoneFlag);
+module RS232_tx(clk, rst, dataIn, tx, dataDoneFlag);
 
-// input/outputs
-input clk, rst, rx;
-output reg [7:0] dataOut;
-output reg dataDoneFlag;
+input clk, rst;
+input [7:0] dataIn;
+input dataDoneFlag;
+output reg tx;
 
 // states
 localparam s_idle = 0;
 localparam s_start = 1;
-localparam s_read = 2;
+localparam s_write = 2;
 localparam s_stop = 3;
 
 // local parameters
 localparam baudClockCycles = 434;
-localparam baudHalf = baudClockCycles / 2;
 
 // local registers
 reg [1:0] STATE;
 reg [8:0] counter;
 reg [2:0] activeBit;
+reg [7:0] latchedData;
 
 
 // logic
@@ -31,51 +31,53 @@ begin
 		STATE <= s_idle;
 		counter <= 0;
 		activeBit <= 0;
-		dataOut <= 0;
-		dataDoneFlag <= 0;
+		tx <= 1;
+		latchedData <= 0;
 	end // if !rst
 	
 	else
 	begin
+	
 		case (STATE)
 		
 		//////////////////////////////////////////////////////////
 		s_idle:
 		begin
-			// what state does
 			counter <= 0;
 			activeBit <= 0;
-			dataDoneFlag <= 0;
+			tx <= 1;
 			
-			// state change condition
-			STATE <= rx ? s_idle : s_start;
-		end // s_idle:
+			STATE <= dataDoneFlag ? s_start : s_idle;
+		end
 		
 		//////////////////////////////////////////////////////////
 		s_start:
 		begin
+			tx <= 0;
+			latchedData <= dataIn;
 			
-			if (counter > baudHalf)
+			if (counter > baudClockCycles)
 			begin
-				STATE <= s_read;
 				counter <= 0;
+				STATE <= s_write;
 			end
 			
 			else
 			begin
+				counter <= counter +1;
 				STATE <= s_start;
-				counter <= counter + 1;
 			end
-		end // s_start
+		end
 		
 		//////////////////////////////////////////////////////////
-		s_read:
+		s_write:
 		begin
+		
 			if (counter > baudClockCycles)
 			begin
 				counter <= 0;
-				dataOut[activeBit] <= rx;
 				activeBit <= activeBit + 1;
+				tx <= latchedData[activeBit];
 				
 				if (activeBit == 7)
 				begin
@@ -84,29 +86,26 @@ begin
 				
 				else
 				begin
-					STATE <= s_read;
+					STATE <= s_write;
 				end
-				
 			end
 			
 			else
 			begin
 				counter <= counter + 1;
-				STATE <= s_read;
+				STATE <= s_write;
 			end
-		end // s_read:
+		
+		end
 		
 		//////////////////////////////////////////////////////////
 		s_stop:
 		begin
-			// set data done flag
-			dataDoneFlag <= 1;
-		
-			// count clock pulses
+			tx <= 1;
+			
 			if (counter > baudClockCycles)
 			begin
 				STATE <= s_idle;
-				counter <= 0;
 			end
 			
 			else
@@ -114,7 +113,7 @@ begin
 				counter <= counter + 1;
 				STATE <= s_stop;
 			end
-		end // s_stop
+		end
 		
 		//////////////////////////////////////////////////////////
 		default
@@ -122,7 +121,9 @@ begin
 			STATE <= s_idle;
 		end
 		
+		
 		endcase
+	
 	end
 
 end // always
