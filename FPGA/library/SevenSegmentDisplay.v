eb7c3e36c1ac417060afe71d0cd2077bb1748315
coolshoeshine <o.enriquez89@gmail.com>
2020-10-20 23:10:41 -0700

    added comments

--- FPGA/library/SevenSegmentDisplay.v
+++ FPGA/library/SevenSegmentDisplay.v
@@ -1,3 +1,21 @@
+/*
+	MadeBy Ozzy
+	Video for this module:
+	https://www.youtube.com/watch?v=4JruzgALCm8
+
+	Support:
+	Digibyte
+	DMgbQqZ9Vr7KrCxd7LrPJE5kWYHZX7DTTW
+	Bitcoin
+	1P4kBPyFoR6xa4sy2hnHL3oY6sg4PrXn6M
+	Litecoin
+	Lc98RWgNJpkYxoEPWux7apP2jA3iu2LMWh
+	Ethereum
+	0xF57a1Fe0E004E4Ac8572e3ba3F4f697823C4e04C
+
+	Contact:
+	madebyozzy@gmail.com
+*/
 
 module SevenSegmentDisplay_RevA(disp0, disp1, disp2, disp3, disp4, disp5);
 
