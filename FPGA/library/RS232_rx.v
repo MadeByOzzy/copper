9902ba0f22ef53c2660d3c8e9c8b27e7e631ed86
coolshoeshine <o.enriquez89@gmail.com>
2021-03-28 15:52:59 -0700

    updated comments

--- FPGA/library/RS232_rx.v
+++ FPGA/library/RS232_rx.v
@@ -4,14 +4,12 @@
 	https://www.youtube.com/watch?v=LKr2qHQY9fo
 
 	Support:
-	Digibyte
-	DMgbQqZ9Vr7KrCxd7LrPJE5kWYHZX7DTTW
-	Bitcoin
-	1P4kBPyFoR6xa4sy2hnHL3oY6sg4PrXn6M
-	Litecoin
-	Lc98RWgNJpkYxoEPWux7apP2jA3iu2LMWh
-	Ethereum
-	0xF57a1Fe0E004E4Ac8572e3ba3F4f697823C4e04C
+    Bitcoin
+    1P4kBPyFoR6xa4sy2hnHL3oY6sg4PrXn6M
+    Digibyte
+    DMgbQqZ9Vr7KrCxd7LrPJE5kWYHZX7DTTW
+    Monero
+    46NQaqW3ZNvUt3DCSxLLLyHpaVMSbwLqS3tHmhVLrafXU3K2crb12qK4PfN7kaHkaLcSTs9cCX1NMPsaCnSMhkRACWzxmax
 
 	Contact:
 	madebyozzy@gmail.com
